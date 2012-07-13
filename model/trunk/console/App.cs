e1a361662f703fcbb36a1d4fc26c2c5a889b6fe6
jimm <jdomingo@green-code.com>
2012-07-13 19:21:34 +0000

    Copied the console's trunk into the model project's trunk.
    git-svn-id: http://Marc-PC/svn/Full@676 85f15e0e-5137-b144-a863-98bd542365a3

---
+++ model/trunk/console/App.cs
@@ -0,0 +1,94 @@
+using Edu.Wisc.Forest.Flel.Util;
+using log4net;
+using System;
+using System.Configuration;
+using System.IO;
+using System.Reflection;
+using System.Text;
+
+using Landis.SpatialModeling;
+using Landis.SpatialModeling.CoreServices;
+using Landis.Core;
+
+namespace Landis
+{
+    public static class App
+    {
+        public static int Main(string[] args)
+        {
+            try {
+                // The log4net section in the application's configuration file
+                // requires the environment variable WORKING_DIR be set to the
+                // current working directory.
+                Environment.SetEnvironmentVariable("WORKING_DIR", Environment.CurrentDirectory);
+                log4net.Config.XmlConfigurator.Configure();
+
+                ConsoleInterface ci = new ConsoleInterface();
+
+                ci.TextWriter = Console.Out;
+
+                string version = GetAppSetting("version");
+                if (version == "")
+                    throw new Exception("The application setting \"version\" is empty or blank");
+                string release = GetAppSetting("release");
+                if (release != "")
+                    release = string.Format(" ({0})", release);
+                ci.WriteLine("LANDIS-II {0}{1}", version, release);
+
+                Assembly assembly = Assembly.GetExecutingAssembly();
+                foreach (object attribute in assembly.GetCustomAttributes(typeof(AssemblyCopyrightAttribute), false))
+                    ci.WriteLine(((AssemblyCopyrightAttribute) attribute).Copyright);
+                ci.WriteLine();
+
+                if (args.Length == 0) {
+                    ci.WriteLine("Error: No scenario file specified.");
+                    return 1;
+                }
+                if (args.Length > 1) {
+                    ci.WriteLine("Error: Extra argument(s) on command line:");
+                    StringBuilder argsList = new StringBuilder();
+                    argsList.Append(" ");
+                    for (int i = 1; i < args.Length; ++i)
+                        argsList.AppendFormat(" {0}", args[i]);
+                    ci.WriteLine(argsList.ToString());
+                    return 1;
+                }
+
+                string appDir = Application.Directory;
+                Landis.PlugIns.IDataset plugIns = (Landis.PlugIns.IDataset) PlugIns.Admin.Dataset.LoadOrCreate(PlugIns.Admin.Dataset.DefaultPath);
+                RasterFactory driverMgr = new RasterFactory(); 
+                Model model = new Model(plugIns, driverMgr);
+                model.Run(args[0], ci);
+                return 0;
+            }
+            catch (ApplicationException exc) {
+                ConsoleInterface ci = new ConsoleInterface();
+                ci.WriteLine(exc.Message);
+                return 1;
+            }
+            catch (Exception exc) {
+
+                ConsoleInterface ci = new ConsoleInterface();
+                ci.WriteLine("Internal error occurred within the program:");
+                ci.WriteLine("  {0}", exc.Message);
+                if (exc.InnerException != null) {
+                    ci.WriteLine("  {0}", exc.InnerException.Message);
+                }
+                ci.WriteLine();
+                ci.WriteLine("Stack trace:");
+                ci.WriteLine(exc.StackTrace);
+                return 1;
+            }
+        }
+
+        //---------------------------------------------------------------------
+
+        public static string GetAppSetting(string settingName)
+        {
+            string setting = ConfigurationManager.AppSettings[settingName];
+            if (setting == null)
+                throw new Exception("The application setting \"" + settingName + "\" is not set");
+            return setting.Trim(null);
+        }
+    }
+}
---
+++ model/trunk/console/ConsoleInterface.cs
@@ -0,0 +1,104 @@
+﻿
+using System;
+using System.IO;
+
+using Edu.Wisc.Forest.Flel.Util;
+using Landis.Core;
+
+namespace Landis
+{
+    /// <summary>
+    /// The user interface for Landis.
+    /// </summary>
+    //public static class UI
+    public class ConsoleInterface
+        : IUserInterface
+    {
+        private static TextWriter writer;
+
+        //---------------------------------------------------------------------
+
+        static ConsoleInterface()
+        {
+            writer = TextWriter.Null;
+        }
+
+        //---------------------------------------------------------------------
+
+        /// <summary>
+        /// Access the user interface as a text writer.
+        /// </summary>
+        /// <remarks>
+        /// Default value is TextWriter.Null.
+        /// </remarks>
+
+        public TextWriter TextWriter
+		{
+			get {
+				return writer;
+			}
+			set {
+				if (value == null)
+					throw new ArgumentNullException("Use System.IO.TextWriter.Null instead of null");
+				writer = value;
+			}
+		}
+
+        //---------------------------------------------------------------------
+
+        public TextWriter ConsoleOut
+        {
+            get
+            {
+                return writer;
+            }
+        }
+
+        //---------------------------------------------------------------------
+        /// <summary>
+        /// Writes a line terminator to the user interface and the application
+        /// log.
+        /// </summary>
+
+        public void WriteLine()
+		{
+			writer.WriteLine();
+			Log.Info(string.Empty);
+		}
+
+        //---------------------------------------------------------------------
+
+        /// <summary>
+        /// Writes a string followed by a line terminator to the user
+        /// interface and the application log.
+        /// </summary>
+
+        public void WriteLine(string text)
+		{
+			writer.WriteLine(text);
+			Log.Info(text);
+		}
+
+        //---------------------------------------------------------------------
+
+        /// <summary>
+        /// Writes a formatted string to the user interface and the application
+        /// log, using the same semantics as System.String.Format.  A line
+        /// terminator is written after the formatted string.
+        /// </summary>
+
+        public void WriteLine(string format, params object[] args)
+		{
+			writer.WriteLine(format, args);
+			Log.Info(format, args);
+		}
+
+        //---------------------------------------------------------------------
+        
+        public ProgressBar CreateProgressMeter(int totalWorkUnits)
+        {
+            return new ProgressBar((uint)totalWorkUnits, System.Console.Out);
+        }
+        
+    }
+}
