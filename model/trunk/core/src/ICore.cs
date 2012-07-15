85d3ae6fb7b72b986eb5dbdf404bd127f5c0edc0
jimm <jdomingo@green-code.com>
2012-07-15 22:10:54 +0000

    Clarified author (developer) affiliation for core's interface.  According to the archive's log:
      http://flel.landis-ii.org/websvn/listing.php?repname=LANDIS
    
    I worked on the original src/plug-ins/ICore interface from 2005 to 2007 (revisions 656 to 1741).  Sri and Rob updated it at PSU when the core was refactored into its API and implementation.  Also, added Apache License info.
    
    
    git-svn-id: http://Marc-PC/svn/Full@679 85f15e0e-5137-b144-a863-98bd542365a3

--- model/trunk/core/src/ICore.cs
+++ model/trunk/core/src/ICore.cs
@@ -1,4 +1,25 @@
-//  Authors: Jimm Domingo, Srinivas S., Robert Scheller; UW-Madison, Portland State University
+// Copyright 2005-2007 University of Wisconsin
+// Copyright 2010-2011 Portland State University
+// Copyright 2012 Green Code LLC
+// All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// Contributors:
+//   Jimm Domingo, UW-Madison, Forest Landscape Ecology Lab
+//   Srinivas S., Portland State University
+//   Robert Scheller, Portland State University
+//   James Domingo, Green Code LLC
 
 using Landis.SpatialModeling;
 using System.IO;
