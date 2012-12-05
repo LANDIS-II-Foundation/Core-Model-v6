19ebd134e43b698a1a17ab6cf46115ade9dc72c5
Brian <brmirand@gmail.com>
2012-12-05 21:21:03 +0000

    Add Debug_Console_rc3 folder and project
    git-svn-id: http://Marc-PC/svn/Full@871 85f15e0e-5137-b144-a863-98bd542365a3

---
+++ Program.cs
@@ -0,0 +1,11 @@
+namespace LANDIS_II_Debug_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Landis.App.Main(args);
+           
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("LANDIS-II Debug Console")]
+[assembly: AssemblyDescription("Debuggin version of the LANDIS-II console")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("UW-Madison, FLEL")]
+[assembly: AssemblyProduct("LANDIS-II")]
+[assembly: AssemblyCopyright("Copyright 2006 University of Wisconsin")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.*")]
