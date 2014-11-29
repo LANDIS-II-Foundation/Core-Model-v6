ff1e01518955dc58112be0964376005131638b07
jimm <jdomingo@green-code.com>
2014-11-29 04:52:58 +0000

    Improved how errors are handled.  An error is now written to the output file as well as the log file.
    git-svn-id: http://Marc-PC/svn/Full@2823 85f15e0e-5137-b144-a863-98bd542365a3

--- sdk/trunk/staging/stage-files.cmd
+++ sdk/trunk/staging/stage-files.cmd
@@ -9,15 +9,15 @@ call %~dp0\initialize-env-vars.cmd
 set LOG=%SCRIPT_DIR%\log.txt
 call :log Working dir: %CD%
 
-if not exist "%STAGING_LIST%" (
-  call :log Missing file: %STAGING_LIST%
-  exit /b 1
-)
-
 rem  Create temporary file where staging output will be written
 set STAGING_OUTPUT_TEMP=%STAGING_OUTPUT:.txt=_tmp.txt%
 type nul > "%STAGING_OUTPUT_TEMP%"
 if exist "%STAGING_OUTPUT%" del "%STAGING_OUTPUT%"
+
+if not exist "%STAGING_LIST%" (
+  call :recordError Missing file: %STAGING_LIST%
+  goto :exitScript
+)
 
 rem  Initially, overwriting of files in the build directory is enabled.
 set OVERWRITE=enabled
@@ -27,12 +27,14 @@ for /f "tokens=* delims=| usebackq" %%A in ("%STAGING_LIST%") do (
   call :stageFile %%A
 )
 
+:exitScript
 rem  Move the temporary output file into place, so if this script is running
 rem  as a task, then this renaming will signal that the task has finished its
 rem  work.
 move "%STAGING_OUTPUT_TEMP%" "%STAGING_OUTPUT%"
 
-goto :eof
+if "%EXIT_CODE%" == "" set EXIT_CODE 0
+exit /b %EXIT_CODE%
 
 rem  ------------------------------------------------------------------------
 rem  Stage a file by copying it to the build directory.
@@ -73,6 +75,15 @@ if "%~1" == "--no-overwrite" (
 ) else (
   call :log Unknown option: %1
 )
+exit /b
+
+rem  ------------------------------------------------------------------------
+rem  Record an error message in the log and the output file.
+
+:recordError
+call :log %*
+echo ERROR: %* >> "%STAGING_OUTPUT_TEMP%"
+set EXIT_CODE=1
 exit /b
 
 rem  ------------------------------------------------------------------------
