1260722a1fa74b7ec2d5c097eae740ceb91e7cff
Vincent Schuster <vicents@pdx.edu>
2017-02-26 10:02:41 -0800

    removed and archived 6.0rc3 files

--- model/docs/6.0rc3/ext-admin/test/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-using System.Reflection;
-
-[assembly: AssemblyDescription("Extensions Administration Tool - Tests")]
-[assembly: AssemblyProduct("Landis-II")]
-[assembly: AssemblyCompany("UW-Madison, Forest Landscape Ecology Lab")]
-[assembly: AssemblyCopyright("Copyright 2005-2006 University of Wisconsin")]
